b49b353a64d2367186f0e6457cdc26ad383a833b
adiarhmn <adiauliarahmn@gmail.com>
2025-01-01 08:44:28 +0800

    Add Midtrans Package

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "@mantine/core/styles.css";
 import "./index.css";
-import { Button, MantineProvider } from "@mantine/core";
+import { Button, Container, MantineProvider } from "@mantine/core";
 import { useState } from "react";
 
 function App() {
@@ -8,8 +8,10 @@ function App() {
 
   return (
     <MantineProvider>
-      <Button onClick={() => setCount((c) => c + 1)}>Increment</Button>
-      <div className="font-bold">{count}</div>
+      <Container>
+        <Button onClick={() => setCount((c) => c + 1)}>Increment</Button>
+        <div className="font-bold text-blue-500">{count}</div>
+      </Container>
     </MantineProvider>
   );
 }
